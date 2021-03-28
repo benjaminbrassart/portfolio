279c028208502fab566d70d706b63a5bfa53b8d3
Benjamin BRASSART <bbrassar@student.42.fr>
2021-03-28 12:13:48 +0200

    Started Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>Home</div>
+  <div class="white--text">
+    <v-avatar size="256">
+      <img src="http://static.bbrassart.fr/img/pic_resume.jpg" />
+    </v-avatar>
+    <div class="text-h2 mt-3">
+      <p>{{ $t("content.home.header-1") }}</p>
+      <p>{{ $t("content.home.header-2") }}</p>
+    </div>
+    <v-divider class="my-5" />
+    <v-container class="text-h6">
+      <p class="text-left">{{ $t("content.home.welcome") }}</p>
+    </v-container>
+  </div>
 </template>
 
 <script lang="ts">
