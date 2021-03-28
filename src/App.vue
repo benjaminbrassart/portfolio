d4ed08edb0e2cce40c3a709689564f19db7248d2
Benjamin BRASSART <bbrassar@student.42.fr>
2021-03-28 05:42:47 +0200

    Added translation

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
   <div id="app">
+    <Navbar />
     <router-view />
   </div>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+import Navbar from "./components/Navbar.vue";
+
+export default Vue.extend({
+  components: {
+    Navbar,
+  },
+});
+</script>
 
 <style lang="scss">
 #app {
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "@mdi/font/css/materialdesignicons.min.css";
 import Vue from "vue";
 import App from "./App.vue";
+import i18n from "./i18n";
 import vuetify from "./plugins/vuetify";
 import "./registerServiceWorker";
 import router from "./router";
@@ -10,5 +11,6 @@ Vue.config.productionTip = false;
 new Vue({
     router,
     vuetify,
+    i18n,
     render: (h) => h(App),
 }).$mount("#app");
