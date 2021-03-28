a28530c2dac23b5ab89703b24fefc50e90bd1ee0
Benjamin BRASSART <bbrassar@student.42.fr>
2021-03-28 14:08:09 +0200

    Used HTTPS instead of HTTP for static content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="white--text">
     <v-avatar size="256">
-      <img src="http://static.bbrassart.fr/img/pic_resume.jpg" />
+      <img src="https://static.bbrassart.fr/img/pic_resume.jpg" />
     </v-avatar>
     <div class="text-h2 mt-3">
       <p>{{ $t("content.home.header-1") }}</p>
--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -2,7 +2,13 @@
   <div>
     <h1 class="text-h4 white--text">Curriculum vitæ</h1>
     <v-divider class="mt-3 mb-2" />
-    <v-btn outlined color="primary">
+    <v-btn
+      outlined
+      color="primary"
+      link
+      href="https://static.bbrassart.fr/pdf/resume.pdf"
+      target="_blank"
+    >
       <v-icon class="mr-2">mdi-file-pdf</v-icon>
       <span>{{ $t("content.resume.download") }}</span>
     </v-btn>
